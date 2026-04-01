b02943ad56f7bf07d34de928c8928caa5c00da23
Jose Paye <carlospayebotero@gmail.com>
2026-04-01 19:02:34 -0400

    refactor: reorganizacion de la estructura para la pantalla1 mediante la creacion de paquetes y clases agregacion de nuevas dependencias en build.gradle.kts para un posible uso

---
+++ app/src/main/java/com/example/recipeplannerlite/views/MainScreen.kt
@@ -0,0 +1,11 @@
+package com.example.recipeplannerlite.views
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RecipeListScreen(
+    modifier: Modifier = Modifier
+){
+
+}
