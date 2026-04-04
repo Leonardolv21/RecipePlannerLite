4cc22bc5fc6cf56e08689291d7a53d664fa859c4
Jose Paye <carlospayebotero@gmail.com>
2026-04-04 18:44:33 -0400

    feat: Todas las secciones de la pantalla1 completadas - Bug en la interaccion con los filtros y la barra de busqueda de recetas - Arreglo mediante el uso de la dependencia lifecycle

---
+++ app/src/main/java/com/example/recipeplannerlite/models/Recipe.kt
@@ -0,0 +1,13 @@
+package com.example.recipeplannerlite.models
+
+data class Recipe(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val emoji: String,
+    val ingredients: List<Ingredient>
+)
+data class Ingredient(
+    val name: String,
+    val quantity: String
+)
